66638d1d0e69ab5255e642631a2a9a4439c146d1
lucasvt22 <lucas22@vt.edu>
2024-02-27 22:45:38 -0500

    Add background and tournament cards

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,54 @@ function App() {
 
   const tournaments = [
     {
-      name: 'Aaa Bbb',
-      weight: 'Lorem Ipsum',
-      date: '19981'
+      name: 'Dubai Open',
+      weight: 'ATP 500',
+      date: 'Feb 26 - Mar 2'
     },
     {
-      name: 'Aaa Bbb',
-      weight: 'Lorem Ipsum',
-      date: '19981'
+      name: 'Mexican Open',
+      weight: 'ATP 500',
+      date: 'Feb 26 - Mar 2'
     },
     {
-      name: 'Aaa Bbb',
-      weight: 'Lorem Ipsum',
-      date: '19981'
+      name: 'Movistar Chile Open',
+      weight: 'ATP 250',
+      date: 'Feb 26 - Mar 3'
+    },
+    {
+      name: 'BNP Paribas Open',
+      weight: 'Masters 1000',
+      date: 'Mar 6 - Mar 17'
+    },
+    {
+      name: 'Miami Open',
+      weight: 'Masters 1000',
+      date: 'Mar 10 - Mar 31'
+    },
+    {
+      name: 'Houston Clay Court Open',
+      weight: 'ATP 250',
+      date: 'Apr 1 - Apr 7'
+    },
+    {
+      name: 'Grand Prix Hassan II',
+      weight: 'ATP 250',
+      date: 'Apr 1 - Apr 7'
+    },
+    {
+      name: 'Millenium Estoril Open',
+      weight: 'ATP 250',
+      date: 'Apr 1 - Apr 7'
+    },
+    {
+      name: 'Rolex Monte-Carlo Masters',
+      weight: 'Masters 1000',
+      date: 'Apr 7 - Apr 14'
+    },
+    {
+      name: 'Barcelona Open',
+      weight: 'ATP 500',
+      date: 'Apr 15 - Apr 21'
     }
   ]
 
@@ -25,17 +60,17 @@ function App() {
     <>
       <h1>Upcoming ATP Events</h1>
       <h2>Check below for the current and upcoming tournaments!</h2>
-      {
-        tournaments.map((tournament, i) => {
-          return (<Card 
-            name={tournament.name} 
-            key={i}
-            weight={tournament.weight} 
-            date={tournamnet.date} />)
-
-        })
-      }
-
+      <div className='cardContainer'>
+        {
+          tournaments.map((tournament, i) => {
+            return (<Card 
+              name={tournament.name} 
+              key={i}
+              weight={tournament.weight} 
+              date={tournament.date} />)
+          })
+        }
+      </div>
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,9 @@ function Card({ name, weight, date })  {
 
     return (
       <section style={{ border: '3px solid white', marginBottom: '8px' }}>
-          <h3>Name: {name}</h3>
-          <h4>Level: {weight}</h4>
-          <p>Dates: {date}</p>
+          <h3>{name}</h3>
+          <h4>{weight}</h4>
+          <p>{date}</p>
       </section>
     )
   }
