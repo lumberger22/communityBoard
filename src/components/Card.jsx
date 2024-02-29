4d8c60a70ddcd4fbd265cb53353d4e62f2424d88
lucasvt22 <lucas22@vt.edu>
2024-02-28 19:55:26 -0500

    Make name of tournament links

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ function Card({ name, weight, date })  {
       <section className="card">
           <img className="card-image" src="#"/>
           <div className="content">
-            <h3>{name}</h3>
+            <h3><a href="#">{name}</a></h3>
             <h4>{weight}</h4>
             <p>{date}</p>
           </div>
