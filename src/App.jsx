ef882772973d16ceea382d7fe8ad63060b794096
lucasvt22 <lucas22@vt.edu>
2024-02-28 20:39:32 -0500

    Add link and image props and tournament specs

--- src/App.jsx
+++ src/App.jsx
@@ -7,59 +7,79 @@ function App() {
     {
       name: 'Dubai Open',
       weight: 'ATP 500',
-      date: 'Feb 26 - Mar 2'
+      date: 'Feb 26 - Mar 2',
+      image: 'images/dubai.jpg',
+      link: 'https://dubaidutyfreetennischampionships.com/'
     },
     {
       name: 'Mexican Open',
       weight: 'ATP 500',
-      date: 'Feb 26 - Mar 2'
+      date: 'Feb 26 - Mar 2',
+      image: 'images/mexican.jpg',
+      link: 'https://abiertomexicanodetenis.com/en/'
     },
     {
       name: 'Movistar Chile Open',
       weight: 'ATP 250',
-      date: 'Feb 26 - Mar 3'
+      date: 'Feb 26 - Mar 3',
+      image: 'images/movistar.jpg',
+      link: 'https://www.chileopen.cl/'
     },
     {
       name: 'BNP Paribas Open',
       weight: 'Masters 1000',
-      date: 'Mar 6 - Mar 17'
+      date: 'Mar 6 - Mar 17',
+      image: 'images/bnpParibas.jpg',
+      link: 'https://bnpparibasopen.com/'
     },
     {
       name: 'Miami Open',
       weight: 'Masters 1000',
-      date: 'Mar 10 - Mar 31'
+      date: 'Mar 10 - Mar 31',
+      image: 'images/miami.jpg',
+      link: 'https://www.miamiopen.com/'
     },
     {
       name: 'Houston Clay Court Open',
       weight: 'ATP 250',
-      date: 'Apr 1 - Apr 7'
+      date: 'Apr 1 - Apr 7',
+      image: 'images/houston.jpg',
+      link: 'https://www.mensclaycourt.com/'
     },
     {
       name: 'Grand Prix Hassan II',
       weight: 'ATP 250',
-      date: 'Apr 1 - Apr 7'
+      date: 'Apr 1 - Apr 7',
+      image: 'images/grandPrix.jpg',
+      link: 'https://www.frmt.ma/'
     },
     {
       name: 'Millenium Estoril Open',
       weight: 'ATP 250',
-      date: 'Apr 1 - Apr 7'
+      date: 'Apr 1 - Apr 7',
+      image: 'images/estoril.jpg',
+      link: 'https://www.millenniumestorilopen.com/'
     },
     {
-      name: 'Rolex Monte-Carlo Masters',
+      name: 'Monte-Carlo Masters',
       weight: 'Masters 1000',
-      date: 'Apr 7 - Apr 14'
+      date: 'Apr 7 - Apr 14',
+      image: 'images/monteCarlo.jpg',
+      link: 'https://montecarlotennismasters.com/en'
     },
     {
       name: 'Barcelona Open',
       weight: 'ATP 500',
-      date: 'Apr 15 - Apr 21'
+      date: 'Apr 15 - Apr 21',
+      image: 'images/barcelona.jpg',
+      link: 'https://www.barcelonaopenbancsabadell.com/en/home/'
     }
   ]
 
   return (
     <>
       <h1>Upcoming ATP Tennis Events</h1>
-      <h3>Check below for the current and upcoming tournaments!</h3>
+      <h2>Check below for the current and upcoming tournaments!</h2>
       <h2>Click on the Names below for more informaion</h2>
       <div className="card-container">
         {
@@ -68,7 +88,9 @@ function App() {
               name={tournament.name} 
               key={i}
               weight={tournament.weight} 
-              date={tournament.date} />)
+              date={tournament.date}
+              image={tournament.image}
+              link={tournament.link} />)
           })
         }
       </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
-function Card({ name, weight, date })  {
+function Card({ name, weight, date, image, link })  {
 
     return (
       <section className="card">
-          <img className="card-image" src="#"/>
+          <img className="card-image" src={image}/>
           <div className="content">
-            <h3><a href="#">{name}</a></h3>
+            <h3><a href={link} target="_blank">{name}</a></h3>
             <h4>{weight}</h4>
             <p>{date}</p>
           </div>
